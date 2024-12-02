fcfcf0fa9178bc48638e6623dbce743b23a75ab7
Ross Searle <ross-s@live.co.uk>
2024-12-02 22:54:31 +0000

    set to respect base path

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     output: process.env.BUILD_MODE ?? undefined,
     experimental: {
         reactCompiler: true,
-    }
+    },
+    basePath: process.env.BASE_PATH ?? undefined,
 };
 
 export default nextConfig;
