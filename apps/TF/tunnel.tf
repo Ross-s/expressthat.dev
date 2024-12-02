6b0dab7fd0477b73aa4343f51b0e4598645346d4
Ross Searle <ross-s@live.co.uk>
2024-12-02 21:35:58 +0000

    create random secret

--- apps/TF/tunnel.tf
+++ apps/TF/tunnel.tf
@@ -1,7 +1,16 @@
+locals {
+    secret = base64encode(random_string.secret.result)
+}
+
+resource "random_string" "secret" {
+    length  = 32
+    special = true
+}
+
 resource "cloudflare_zero_trust_tunnel_cloudflared" "expressthat-apps" {
   account_id = var.cloudflare_account_id
   name       = "expressthat-apps"
   config_src = "cloudflare"
-  secret = ""
+  secret = local.secret
 }
 
