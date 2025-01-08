c6103d614b997ee71b290c187ab5312b5f2b9505
Ross Searle <ross-s@live.co.uk>
2025-01-08 18:08:36 +0000

    changed to remix

--- apps/server/admin/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { nextJsConfig } from "@expressthat/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
--- apps/server/admin/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: process.env.BUILD_MODE ?? undefined,
-    experimental: {
-        reactCompiler: true,
-    },
-};
-
-export default nextConfig;
