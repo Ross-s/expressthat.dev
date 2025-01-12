000aecfecd0353b495903aacd99e5d8ddbb3f140
Ross Searle <ross-s@live.co.uk>
2025-01-12 05:31:07 +0000

    Changed Url

--- apps/server/TF/proxyRules.tf
+++ apps/server/TF/proxyRules.tf
@@ -1,5 +1,5 @@
 resource "nginxproxymanager_proxy_host" "admin" {
-  domain_names            = ["admin.${var.domain_name}"]
+  domain_names            = ["app.${var.domain_name}"]
   forward_scheme          = "http"
   forward_host            = var.internal_host
   forward_port            = 3002
